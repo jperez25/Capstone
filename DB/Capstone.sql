50372aa5cd1681deb01bdf0d5a18d298848cd8a9
jperez25 <jova9991@gmail.com>
2019-01-29 11:45:51 -0600

    DB update 1.5

--- DB/Capstone.sql
+++ DB/Capstone.sql
@@ -27,12 +27,13 @@ DROP TABLE IF EXISTS `appoiment`;
 CREATE TABLE `appoiment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
-  `hour` date DEFAULT NULL,
+  `hour` int(10) DEFAULT NULL,
+  `duration` varchar(45) DEFAULT NULL,
   `description` varchar(45) DEFAULT NULL,
   `attendance` tinyint(4) DEFAULT NULL,
   `diagnostics` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +42,7 @@ CREATE TABLE `appoiment` (
 
 LOCK TABLES `appoiment` WRITE;
 /*!40000 ALTER TABLE `appoiment` DISABLE KEYS */;
+INSERT INTO `appoiment` VALUES (1,'2019-02-02',9,'1','descriptiopn',NULL,NULL),(2,'2019-02-02',10,'2','description',NULL,NULL),(3,'2019-02-03',9,'2','description',NULL,NULL),(4,'2019-02-04',11,'2','description',NULL,NULL);
 /*!40000 ALTER TABLE `appoiment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,13 +56,13 @@ DROP TABLE IF EXISTS `appoiments_per_patient`;
 CREATE TABLE `appoiments_per_patient` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `patient_id` int(11) DEFAULT NULL,
-  `appoiment` int(11) DEFAULT NULL,
+  `appoiment_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `patient_app_idx` (`appoiment`),
+  KEY `patient_app_idx` (`appoiment_id`),
   KEY `patient_id_idx` (`patient_id`),
-  CONSTRAINT `patient_app` FOREIGN KEY (`appoiment`) REFERENCES `appoiment` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `patient_app` FOREIGN KEY (`appoiment_id`) REFERENCES `appoiment` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,6 +71,7 @@ CREATE TABLE `appoiments_per_patient` (
 
 LOCK TABLES `appoiments_per_patient` WRITE;
 /*!40000 ALTER TABLE `appoiments_per_patient` DISABLE KEYS */;
+INSERT INTO `appoiments_per_patient` VALUES (1,1,1),(4,1,4);
 /*!40000 ALTER TABLE `appoiments_per_patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,6 +86,7 @@ CREATE TABLE `conditions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `description` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -97,34 +101,6 @@ LOCK TABLES `conditions` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `conditions_per_patient`
---
-
-DROP TABLE IF EXISTS `conditions_per_patient`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `conditions_per_patient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `patient_id` int(11) DEFAULT NULL,
-  `condition` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `patient_idx` (`patient_id`),
-  KEY `conditon_idx` (`condition`),
-  CONSTRAINT `conditon` FOREIGN KEY (`condition`) REFERENCES `conditions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `conditions_per_patient`
---
-
-LOCK TABLES `conditions_per_patient` WRITE;
-/*!40000 ALTER TABLE `conditions_per_patient` DISABLE KEYS */;
-/*!40000 ALTER TABLE `conditions_per_patient` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `eye`
 --
 
@@ -135,7 +111,7 @@ CREATE TABLE `eye` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `color` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,8 +120,31 @@ CREATE TABLE `eye` (
 
 LOCK TABLES `eye` WRITE;
 /*!40000 ALTER TABLE `eye` DISABLE KEYS */;
-INSERT INTO `eye` VALUES (1,'blue');
+INSERT INTO `eye` VALUES (1,'blue'),(2,'brown'),(3,'green');
 /*!40000 ALTER TABLE `eye` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medication`
+--
+
+DROP TABLE IF EXISTS `medication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medication` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `medication` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medication`
+--
+
+LOCK TABLES `medication` WRITE;
+/*!40000 ALTER TABLE `medication` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medication` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -188,9 +187,9 @@ CREATE TABLE `patient` (
   `height` varchar(45) DEFAULT NULL,
   `eye_colour` int(11) DEFAULT NULL,
   `ssn` varchar(45) DEFAULT NULL,
-  `conditions` int(11) DEFAULT NULL,
-  `prev_conditions` int(11) DEFAULT NULL,
-  `appoiment` int(11) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `phone_number` varchar(13) DEFAULT NULL,
+  `insurance` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `eye_idx` (`eye_colour`),
   CONSTRAINT `eye` FOREIGN KEY (`eye_colour`) REFERENCES `eye` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -203,8 +202,66 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES (1,'Jovanny','Perez','170','68',1,'111224444',NULL,NULL,NULL);
+INSERT INTO `patient` VALUES (1,'Jovanny','Perez','170','68',1,'111224444','1996-08-24',NULL,NULL);
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient_medication`
+--
+
+DROP TABLE IF EXISTS `patient_medication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient_medication` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_id` int(11) DEFAULT NULL,
+  `medication_id` int(11) DEFAULT NULL,
+  `current` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pat_id_idx` (`patient_id`),
+  KEY `med_id_idx` (`medication_id`),
+  CONSTRAINT `med_id` FOREIGN KEY (`medication_id`) REFERENCES `medication` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `pat_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient_medication`
+--
+
+LOCK TABLES `patient_medication` WRITE;
+/*!40000 ALTER TABLE `patient_medication` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient_medication` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patients_medical_history`
+--
+
+DROP TABLE IF EXISTS `patients_medical_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patients_medical_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_id` int(11) DEFAULT NULL,
+  `condition` int(11) DEFAULT NULL,
+  `current` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `patient_idx` (`patient_id`),
+  KEY `conditon_idx` (`condition`),
+  CONSTRAINT `conditon` FOREIGN KEY (`condition`) REFERENCES `conditions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patients_medical_history`
+--
+
+LOCK TABLES `patients_medical_history` WRITE;
+/*!40000 ALTER TABLE `patients_medical_history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patients_medical_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -218,7 +275,7 @@ CREATE TABLE `positions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `position` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -227,36 +284,8 @@ CREATE TABLE `positions` (
 
 LOCK TABLES `positions` WRITE;
 /*!40000 ALTER TABLE `positions` DISABLE KEYS */;
-INSERT INTO `positions` VALUES (1,'nurse');
+INSERT INTO `positions` VALUES (1,'nurse'),(2,'admin'),(3,'doctor');
 /*!40000 ALTER TABLE `positions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `prev_conditions_per_patient`
---
-
-DROP TABLE IF EXISTS `prev_conditions_per_patient`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `prev_conditions_per_patient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `patient_id` int(11) DEFAULT NULL,
-  `prev_condition` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `patient_idx` (`patient_id`),
-  KEY `prev_condition_idx` (`prev_condition`),
-  CONSTRAINT `patient_prev` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `prev_condition` FOREIGN KEY (`prev_condition`) REFERENCES `conditions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `prev_conditions_per_patient`
---
-
-LOCK TABLES `prev_conditions_per_patient` WRITE;
-/*!40000 ALTER TABLE `prev_conditions_per_patient` DISABLE KEYS */;
-/*!40000 ALTER TABLE `prev_conditions_per_patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -332,7 +361,7 @@ CREATE TABLE `users` (
   KEY `position_idx` (`position`),
   CONSTRAINT `office` FOREIGN KEY (`office`) REFERENCES `office` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `position` FOREIGN KEY (`position`) REFERENCES `positions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -341,7 +370,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'jPerez_16','$2a$04$m0as9WAXlo1Qx/aIRdlHPunHYlu6SMllt78B9fz5k6WptdLvQQgxy','Jovanny Perez',1,1);
+INSERT INTO `users` VALUES (2,'jPerez_16','$2a$04$m0as9WAXlo1Qx/aIRdlHPunHYlu6SMllt78B9fz5k6WptdLvQQgxy','Jovanny Perez',1,1),(3,'ewagner04','$2a$04$md2CbGvqtKtTB0ahNLKMgeet2VPy8EwBpWkW7T1b1g1VRdyRkI1hi','Eric Wagner',1,1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -362,4 +391,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-24 11:44:18
+-- Dump completed on 2019-01-29 11:45:27
